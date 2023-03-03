622b78d375f0d05360da01b438d782b917a4a00a
you@example.com <you@example.com>
2023-03-03 11:38:16 +0100

    Updated pages

---
+++ pages/articles/index.vue
@@ -0,0 +1,9 @@
+<script setup>
+</script>
+
+<template>
+	<h1>contact</h1>
+	<div>
+		<NuxtLink to="/">back</NuxtLink>
+	</div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,10 @@
+<script setup>
+</script>
+
+<template>
+	<h1>index</h1>
+	<div>
+		<NuxtLink to="/contact">contact</NuxtLink>
+		<NuxtLink to="/articles">articles</NuxtLink>
+	</div>
+</template>
